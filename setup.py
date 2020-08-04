7586837b0db1f71c5ad1ee8675a0293f3ced2c49
Andrew <andrew.c.reed88@gmail.com>
2020-08-04 17:22:35 -0400

    Added the setup.py script as well as a Jupyter notebook with an example use of the tool.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="ooinet",
+      version="0.1",
+      author="Andrew Reed",
+      packages=["ooinet"],
+      url="https://github.com/reedan88/OOINet",
+      license="MIT",
+      description="Tool to facilitate M2M requests with OOINet.",
+      )
