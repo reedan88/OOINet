5e8b08ac8d5bea0b02967b2a9c3369c2ed1567c0
Andrew Reed <areed@whoi.edu>
2022-07-14 14:33:17 -0400

    Updating LICENSE, pyproject, and setup files

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
-from setuptools import setup
+import setuptools
 
-setup(name="ooinet",
-      version="0.1",
-      author="Andrew Reed",
-      packages=["ooinet"],
-      url="https://github.com/reedan88/OOINet",
-      license="MIT",
-      description="Tool to facilitate M2M requests with OOINet.",
-      )
+with open(README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ooinet",
+    version="0.0.1a",
+    author="Andrew Reed",
+    authoer_email="areed@whoi.edu",
+    descriptio"="A package for interacting with the Ocean Observatories Initiative (OOI) Machine-2-Machine (M2M) API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPLv3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "ooinet"},
+    packages=setuptools.find_packages(where="ooinet"),
+    python_requires=">=3.9.7"
+)
